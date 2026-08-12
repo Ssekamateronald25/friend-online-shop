3854e2150340f8307b9a336a71cd39ac633eb959
Ssekamatte Ronald <ssekamateronald25@gmail.com>
2026-08-12 11:45:43 -0600

    Connect website to Supabase

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://kbnpeyqbkbtnbnegsjsb.supabase.co";
+const SUPABASE_KEY = "sb_publishable_NHziq1_9j3vhlCeuvThs1g__VEaUHtm";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
